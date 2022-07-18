5e29b6f1a845825f9275e2744581dfc2d41b7433
Zach Eisenhutt <zach35@ksu.edu>
2022-07-18 12:00:14 -0500

    Starting to actually parse float values.

--- test_feb_cmds.py
+++ test_feb_cmds.py
@@ -6,17 +6,23 @@ from warnings import warn
 
 @pytest.fixture(scope="session")
 def feb_connection():
-    """A pytest test fixture that provides a stable connection to the FEB.
+    """Make a test fixture for pytest to reuse the same feb_connnection
+    for all tests."""
+    # pylint: disable=import-outside-toplevel
+    sock = make_feb_socket()
+    yield sock
+    sock.close()
+
+def make_feb_socket():
+    """Make connection to the FEB.
     Assumes that FEB has address 172.16.10.10 and port 5002.
     """
-    # pylint: disable=import-outside-toplevel
     import socket
     ip, port = ('172.16.10.10', 5002)
     sock = socket.socket()
     sock.connect((ip, port))
     sock.settimeout(3.0)
-    yield sock
-    sock.close()
+    return sock
 
 def test_ID(feb_connection): # pylint: disable=redefined-outer-name
     s = sockexpect.SockExpect(feb_connection)
@@ -33,9 +39,23 @@ def test_ID(feb_connection): # pylint: disable=redefined-outer-name
 def test_ADC(feb_connection): # pylint: disable=redefined-outer-name
     s = sockexpect.SockExpect(feb_connection)
     s.send(b'ADC\r\n')
+    s.expect(br'1.2v_Pos *')
+    print("Before: " + repr(s.before))
+    print("Match: " + repr(s.match))
+    print("data: " + repr(s.data))
+    s.expect(br'-*[0-9.]+')
+    print("Before: " + repr(s.before))
+    print("Match: " + repr(s.match))
+    v = float(s.match.group(0))
+    # TODO: compare v to expected values 1.2
+    # TODO: repeat above for 1.8v_Pos, etc.....
+    print(" *** The voltage is """, v, " !!!!")
+    print("data: " + repr(s.data))
     s.expect(br'Temp_C.*\n')
-    print(s.before+s.after)
-    s.after.clear()
+    print("Before: " + repr(s.before))
+    print("Match: " + repr(s.match))
+    print("data: " + repr(s.data))
+    s.data.clear()
 
 def test_SD(feb_connection):
     s = sockexpect.SockExpect(feb_connection)
@@ -43,11 +63,11 @@ def test_SD(feb_connection):
     nfailures = 0
     for ifpga in range(1,5):
         s.before.clear()
-        s.after.clear()
+        s.data.clear()
         s.sendline(bytes(f'SD {ifpga}', 'ascii'))
         s.expect(br'Test.*\n')
-        print(f'SD {ifpga}: {s.before+s.after}')
-        if b'Tested Okay' not in s.after:
+        print(f'SD {ifpga}: Before: {s.before}, Data: {s.data}')
+        if b'Tested Okay' not in s.data:
             warn(f'FPGA {ifpga} failed SD test')
             nfailures += 1
     assert nfailures == 0
